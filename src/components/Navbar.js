ac80f4d70546768831f464760c170313629b70e6
Bantas93 <bantaz.mtdn@gmail.com>
2023-09-13 22:11:23 +0700

    push

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,20 +9,54 @@ const Navbar = () => {
     };
   };
   return (
-    <nav className="d-flex justify-content-center">
-      <NavLink style={navLinkStyles} to="/" className="m-2 p-1">
-        Home
-      </NavLink>
-      <NavLink style={navLinkStyles} to="About" className="m-2 p-1">
-        About
-      </NavLink>
-      <NavLink style={navLinkStyles} to="/Experience" className="m-2 p-1">
-        Experience
-      </NavLink>
-      <NavLink style={navLinkStyles} to="/Contact" className="m-2 p-1">
-        Contact
-      </NavLink>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <NavLink style={navLinkStyles} to="#" className="navbar-brand">
+          Bantas
+        </NavLink>
+        <button
+          class="navbar-toggler navbar-dark"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <NavLink style={navLinkStyles} to="/" className="m-2 p-1">
+              Home
+            </NavLink>
+            <NavLink style={navLinkStyles} to="About" className="m-2 p-1">
+              About
+            </NavLink>
+            <NavLink style={navLinkStyles} to="/Experience" className="m-2 p-1">
+              Experience
+            </NavLink>
+            <NavLink style={navLinkStyles} to="/Contact" className="m-2 p-1">
+              Contact
+            </NavLink>
+          </div>
+        </div>
+      </div>
     </nav>
+    // <nav className="d-flex justify-content-center">
+    // <NavLink style={navLinkStyles} to="/" className="m-2 p-1">
+    //   Home
+    // </NavLink>
+    // <NavLink style={navLinkStyles} to="About" className="m-2 p-1">
+    //   About
+    // </NavLink>
+    // <NavLink style={navLinkStyles} to="/Experience" className="m-2 p-1">
+    //   Experience
+    // </NavLink>
+    // <NavLink style={navLinkStyles} to="/Contact" className="m-2 p-1">
+    //   Contact
+    // </NavLink>
+    // </nav>
   );
 };
 export default Navbar;
