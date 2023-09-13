9574c9e2fd011c9c6d8bd34db28fae64d3c8df20
Bantas93 <bantaz.mtdn@gmail.com>
2023-09-13 21:43:57 +0700

    push

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,5 +1,21 @@
 const Experience = () => {
-  return <div className="bg-Experience text-center pt-5"></div>;
+  return (
+    <div className="bg-Experience text-center pt-5">
+      <h1>Experience</h1>
+      <div class="alert alert-success" role="alert">
+        <h4 class="alert-heading">Bagus sekali!</h4>
+        <p>
+          Aww ya, Anda berhasil membaca pesan peringatan penting ini. Contoh
+          teks ini akan berjalan sedikit lebih lama sehingga Anda dapat melihat
+          cara kerja spasi dalam peringatan dengan konten semacam ini.
+        </p>
+        <p class="mb-0">
+          Kapanpun Anda membutuhkannya, pastikan untuk menggunakan utilitas
+          margin untuk menjaga semuanya tetap bagus dan rapi.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Experience;
