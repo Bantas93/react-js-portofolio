cc5319fb64a783f6a4965af0ca0b33235b74cc5f
Bantas93 <bantaz.mtdn@gmail.com>
2023-09-21 21:11:49 +0700

    Pushed

--- src/components/Program.js
+++ src/components/Program.js
@@ -13,10 +13,9 @@ const Program = () => {
           <div class="card" style={{ width: "18rem" }}>
             <img src={satu} class="card-img-top" alt="xxx" />
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
+              <h5 class="card-title">App Exercise</h5>
               <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                Aplikasi ini berisi kumpulan latihan saya belajar React Js.
               </p>
               <Link
                 to="https://exercise-tau.vercel.app/"
